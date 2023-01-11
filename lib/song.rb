3962d20255b72ce5d3295ca287ebba74de135060
alexbotha <alexbotha@hotmail.co.uk>
2023-01-11 16:38:24 +0000

    commit tree 5c7775b78eb2869dcad906b07b2df27e070d2af9

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,16 @@
 require 'pry'
 
 class Song
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :artist
 
-  extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
